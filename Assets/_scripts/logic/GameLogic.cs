3ed33a2731972207a135f834d317f8bb41bca7eb
afternooncg <afternooncg@126.com>
2017-06-01 08:01:16 +0800

    create

---
+++ Assets/_scripts/logic/GameLogic.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLogic : MonoBehaviour {
+    
+
+    public enum GameLogicState
+    {
+        NONE,
+        LOADING,
+        START,
+        PLAYING,
+        OVER,
+        HELP,
+
+    }
+
+    private GameLogicState m_state = GameLogicState.NONE;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void UpdateLogicState (GameLogicState state) 
+    {
+        if (state == m_state)
+            return;
+
+        switch (state)
+        {
+            case GameLogicState.LOADING:
+
+                break;
+
+            case GameLogicState.START:
+                SceneManager.LoadSceneAsync("start");
+                break;
+
+            case GameLogicState.PLAYING:
+                SceneManager.LoadSceneAsync("main");
+                break;
+
+            case GameLogicState.OVER:
+                SceneManager.LoadSceneAsync("over");
+                break;
+
+            case GameLogicState.HELP:
+                SceneManager.LoadSceneAsync("help");
+                break;
+        
+        
+        }
+		
+	}
+
+
+}
---
+++ Assets/_scripts/tools/ShowCodeExecTime.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowCodeExecTime  {
+
+    static private ShowCodeExecTime m_instance;
+
+    float m_subtime;
+
+    public ShowCodeExecTime Instance
+    {
+        get {
+
+            if (m_instance == null)
+                m_instance = new ShowCodeExecTime();
+            
+            return m_instance; 
+        
+            }
+        
+    }
+    
+
+    public ShowCodeExecTime()
+    {
+
+    
+    }
+
+    public void Begin()
+    {
+        m_subtime = Time.realtimeSinceStartup;
+    }
+
+    public void End()
+    {
+        m_subtime = Time.realtimeSinceStartup - m_subtime;
+        Debug.Log("ShowCodeExecTime:" + m_subtime);
+    }
+
+
+	
+}
